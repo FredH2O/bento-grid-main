157a94542c3a0bb977c7f78d520228289ebc03a5
Fred <huertaserick@yahoo.ie>
2024-12-05 14:59:55 +0000

    okay

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // If you have an index.html file
+    "./src/**/*.{js,jsx,ts,tsx}", // If you're using JavaScript, JSX, or other files in src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
